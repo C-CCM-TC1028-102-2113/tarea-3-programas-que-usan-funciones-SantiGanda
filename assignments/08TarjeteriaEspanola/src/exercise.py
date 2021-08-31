72397c031bb4e21e8ab426e4c9d8ddfcec610b8e
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-30 22:03:35 -0500

    Se utiliza para obtener un máximo de cosas

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,17 @@
 
+def maxi(pli, plu):
+    if pli< plu:
+        return pli*12
+    
+    elif plu<pli:
+        return plu*35
+         
+
 def main():
-    #escribe tu código abajo de esta línea
+    pliegos=int(input('Dame la cantidad de pliegos de papel albanene: '))
+    plumones=int(input('Dame la cantidad de plumones '))
+    tarjetas=maxi(pliegos, plumones)
+    print('El número máximo de tarjetas que puedes hacer es: '+str(tarjetas))
     pass
 
 if __name__=='__main__':
