1ca6f52473643340d3d1ceb254c6b714c4f38f5d
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-30 23:01:58 -0500

    Te dice si un año es bisiesto o no

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,18 @@
+def check(a):
+    if a%100==0 and a%400!=0 or a%4!=0:
+        return 'No es bisiesto'
+    elif a%4==0 and a%100!=0:
+        return 'Es bisiesto'
+    else:
+        return 'Selecciona un valor apropiado'
 
 def main():
-    #escribe tu código abajo de esta línea
+    año=int(input('Dame un año: '))
+    respuesta=check(año)
+    print(respuesta)
+
     pass
 
 if __name__=='__main__':
     main()
+
