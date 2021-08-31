386c640b1817bbb3c7cd2c3bbd2ab6733210a58a
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-30 21:48:24 -0500

    Realiza dos funciones que dan como resultado un volumen

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,21 @@
+def area(bas, alt):
+    return (bas*alt)/2
+     
+def volumen(ar, prof):
+    return ar*prof
+    
+
 
 def main():
     #escribe tu código abajo de esta línea
+    base=float(input('Dame la base '))
+    altura=float(input('Dame la altura '))
+    profundidad=float(input('Dame la profundiad '))
+    aria=area(base, altura)
+    vol=volumen(aria, profundidad)
+    print('El volumen del prisma es: '+str(vol))
+
+
     pass
 
 if __name__=='__main__':
